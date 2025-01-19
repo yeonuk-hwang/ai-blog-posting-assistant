7b0908941e82100a924e298ba722905250daf220
Yeonuk Hwang <charlie.hwang131@gmail.com>
2025-01-20 08:59:24 +0900

    feat(server): add health check

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PostsModule } from './posts/posts.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
@@ -8,6 +9,7 @@ import { PostsModule } from './posts/posts.module';
       isGlobal: true,
     }),
     PostsModule,
+    HealthModule,
   ],
 })
 export class AppModule {}
