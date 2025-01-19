c4e145719e478ba2016768b6e82a2dc33fdf3e83
Yeonuk Hwang <charlie.hwang131@gmail.com>
2025-01-20 03:53:51 +0900

    feat(web): styling main page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,6 +4,7 @@ import { PostsServiceImplementation } from "./services/posts.service";
 import Markdown from "react-markdown";
 import remarkBreaks from "remark-breaks";
 import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
 
 const httpService = new HttpServiceImplementation("http://localhost:3000/");
 const postsService = new PostsServiceImplementation(httpService);
@@ -18,13 +19,26 @@ function App() {
   }
 
   return (
-    <>
-      <div className="prose">
-        <Markdown remarkPlugins={[remarkBreaks]}>{post}</Markdown>
-      </div>
-      <input onChange={(e) => setGuide(e.target.value)} value={guide} />
-      <Button onClick={generatePost}>generate post</Button>
-    </>
+    <div className="flex w-screen h-screen">
+      <section className="prose border flex-1 max-w-full p-6">
+        {post ? (
+          <Markdown remarkPlugins={[remarkBreaks]}>{post}</Markdown>
+        ) : (
+          <p className="text-gray-500">Please generate a post</p>
+        )}
+      </section>
+      <section className="w-1/3 max-w-md p-6">
+        <Textarea
+          placeholder="블로그 포스팅 가이드를 입력하세요"
+          className="h-1/3"
+          onChange={(e) => setGuide(e.target.value)}
+          value={guide}
+        />
+        <section className="mt-4">
+          <Button onClick={generatePost}>블로그 포스팅 생성</Button>
+        </section>
+      </section>
+    </div>
   );
 }
 
