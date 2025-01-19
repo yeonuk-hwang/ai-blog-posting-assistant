75d0adbac8bb92d4fb0e11f24f88c2a2759d63d9
Yeonuk Hwang <charlie.hwang131@gmail.com>
2025-01-20 01:20:46 +0900

    feat(web): render post with formatting
    - install tailwind css

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import { HttpServiceImplementation } from "./services/http.service";
 import { PostsServiceImplementation } from "./services/posts.service";
+import Markdown from "react-markdown";
+import remarkBreaks from "remark-breaks";
 
 const httpService = new HttpServiceImplementation("http://localhost:3000/");
-
 const postsService = new PostsServiceImplementation(httpService);
 
 function App() {
@@ -17,7 +18,9 @@ function App() {
 
   return (
     <>
-      <div>{post}</div>
+      <div className="prose">
+        <Markdown remarkPlugins={[remarkBreaks]}>{post}</Markdown>
+      </div>
       <input onChange={(e) => setGuide(e.target.value)} value={guide} />
       <button onClick={generatePost}>generate post</button>
     </>
