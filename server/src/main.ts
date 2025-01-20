1cd117e3b7f8404c443425619bafb83007e00154
Yeonuk Hwang <charlie.hwang131@gmail.com>
2025-01-20 11:17:27 +0900

    feat(server): enable all cors temporary

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log(process.env.FRONT_DOMAIN);
+
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: [process.env.FRONT_DOMAIN],
+      origin: ['*'],
     },
   });
   await app.listen(process.env.PORT ?? 3000);
