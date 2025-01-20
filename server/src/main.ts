125c7cb8fe931ce1893375a2641be16790481dc7
Yeonuk Hwang <charlie.hwang131@gmail.com>
2025-01-20 11:29:11 +0900

    fix(server): enable cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  console.log(process.env.FRONT_DOMAIN);
+  function convertToRegex(domain: string): RegExp {
+    return new RegExp(`^https:\\/\\/${domain.replace('.', '\\.')}$`);
+  }
 
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: ['*'],
+      origin: [convertToRegex(process.env.FRONT_DOMAIN)],
     },
   });
   await app.listen(process.env.PORT ?? 3000);
